6df82e5445bd96f9ebd8fc7a8c728a47ed0be737
Chebrik <134996732+Chebrik@users.noreply.github.com>
2023-06-08 12:59:46 +0700

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject fireballPrefab;
+    public Transform attackPoint;
+
+    private int health;
+    private int coins;
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        print("Здоровье игрока: " + health);
+
+
+    }
+
+    public void CollectCoins()
+    {
+        coins += 1;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(fireballPrefab, attackPoint.position, attackPoint.rotation);
+        }
+
+    }
+}
